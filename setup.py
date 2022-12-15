f29b89987a16826989ad094898b5e749de3bf1ef
aalteirac <anthony@alteirac.com>
2022-12-15 17:49:54 +0100

    version

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 setuptools.setup(
     name="streamlit-login-page",
     #begin 
-    version="", 
+    vversion="0.1.1", 
     #end
     author="Anthony Alteirac",
     author_email="anthony@alteirac.com",
