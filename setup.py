fc6896f2bb283700bcb8e6b20629a31f97c74b40
aalteirac <anthony@alteirac.com>
2022-12-15 17:51:22 +0100

    auto-release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ long_description = (this_directory / "README.md").read_text()
 setuptools.setup(
     name="streamlit-login-page",
     #begin 
-    version="", 
+    version="0.0.1", 
     #end
     author="Anthony Alteirac",
     author_email="anthony@alteirac.com",
